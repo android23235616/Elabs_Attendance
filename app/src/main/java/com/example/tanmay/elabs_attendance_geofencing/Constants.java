b95c482250b6e78e2c3be601103b6fdf37f1017c
android23235616 <majumdartanmay68@gmail.com>
2017-02-18 10:04:10 +0530

    Firebase Registration Added.              Buttons added  Firebase Verified Attendence System Added.

--- app/src/main/java/com/example/tanmay/elabs_attendance_geofencing/Constants.java
+++ app/src/main/java/com/example/tanmay/elabs_attendance_geofencing/Constants.java
@@ -13,9 +13,9 @@ public class Constants {
     public static String Not_Entered = "232131321";
     public static final double Lat2 = 20.35138524475558;
     public static final double Lng2= 85.82143073306530;
-    public static final double Lat = 20.355526715211838; // lt1
-    public static final double Lng = 85.82077033817768; //lt1
-    public static final int radius = 5;
+    public static final double Lat = 20.35550974091063; // lt1
+    public static final double Lng = 85.82042466849089; //lt1
+    public static final int radius = 20;
     public static final double Lat1 = 20.351343;
     public static final double Lng1= 85.821465;
     public static final String subject_Changed_Condition="Choose Your class";
